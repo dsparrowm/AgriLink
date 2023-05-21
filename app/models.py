6b7a9927831794205e2527fe9db4db633dadd962
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-05-21 15:03:15 -0700

    fixed errors on farmer model

--- app/models.py
+++ app/models.py
@@ -3,9 +3,9 @@ from app import db
 class User(db.Model):
     __tablename__ = "User"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(120), index=True, unique=False)
-    password_hash = db.Column(db.String(128), nullable=True)
+    name = db.Column(db.String(120), nullable=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
@@ -20,8 +20,9 @@ class Farmer(db.Model):
     __tablename__ = "Farmer"
     id = db.Column(db.Integer, primary_key=True)
     location = db.Column(db.String, nullable=True)
-    name = db.Column(db.String(120), nullable=True)
+    name = db.Column(db.String(120), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+    phone = db.Column(db.String(40), nullable=True)
 
     def __repr__(self):
         return "<Farmer {}>".format(self.name)
