472fe5f718d9e027cd352c65314b2276655a717f
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-05-21 14:29:28 -0700

    i added models for the database, usin SQLITE for now

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = "User"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), index=True, unique=False)
+    password_hash = db.Column(db.String(128), nullable=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Farmer(db.Model):
+    __tablename__ = "Farmer"
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String, nullable=True)
+    name = db.Column(db.String(120), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'))
+
+    def __repr__(self):
+        return "<Farmer {}>".format(self.name)
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,50 @@
-from flask import render_template, request
+"""This module contains routes and view functions for imteracting
+with the fromtend
+"""
+from flask import request, jsonify
 from app import app
+from app.models import User
 
-@app.route('/', strict_slashes=False)
-@app.route('/index', strict_slashes=False)
-def index():
-    return render_template('index.html')
 
-@app.route('/login', methods=['GET, POST'], strict_slashes=False)
+@app.route('/login', methods=['POST'])
 def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    return render_template('login.html')
+    data = request.get_json()
+    form_email = data.get('email')
+    form_password = data.get('password')
+    user = User.query.filter_by(email=user_email).first()
+    if user is None or not user.check_password(form_password):
+        return jsonify({'message': 'Invalid email or password'})
+    else:
+        return jsonify({'message': 'Login successful'})
+
+@app.route('/register', methods=['POST'])
+def register():
+    """Handles registration form sent from the frontend
+    and stores in the database
+    """
+    data = request.get_json()
+    form_name = data.get('name')
+    form_email = data.get('email')
+    form_password = data.get('password')
+    form_role = data.get('role')
+    user_exists = User.query.filter_by(email=form_email).first()
+    if user_exists:
+        return jsonify({'message': 'user already exists'})
+    if form_role == 'farmer':
+        form_location = data.get('location')
+        form_phone = data.get('phone')
+        user = User(name=form_name, email=form_email,
+                    password_harsh=user.set_password(form_password))
+        db.session.add(user)
+        db.session.commit()
+        farmer = Farmer(name=form_name,location=form_location,
+                        phone=form_phone, user_id=user.id)
+        db.session.add(farmer)
+        db.session.commit()
+        return jsonify({'message': 'Registration successful'}), 200
+    elif role == 'buyer':
+        user = User(name=form_name, email=form_email,
+                    password_hash=user.set_password(form_password))
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'message': 'Registration successful'}), 200
