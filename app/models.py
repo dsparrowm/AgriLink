caea82f1bab8f0cb3117d9593137a5994dd59e2a
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-05-22 13:11:58 -0700

    fixed hash function

--- app/models.py
+++ app/models.py
@@ -11,11 +11,6 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.name)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
 
 class Farmer(db.Model):
     __tablename__ = "Farmer"
--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,8 @@ with the fromtend
 from flask import request, jsonify
 from app import app
 from app.models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 @app.route('/status', methods=['GET'])
 def status():
@@ -16,10 +18,11 @@ def login():
     form_email = data.get('email')
     form_password = data.get('password')
     user = User.query.filter_by(email=user_email).first()
-    if user is None or not user.check_password(form_password):
-        return jsonify({'message': 'Invalid email or password'})
+    if user is None or check_password_hash(user.password_hash, form_password) != True:
+        return jsonify({'message': 'Invalid email or password'}), 401
     else:
-        return jsonify({'message': 'Login successful'})
+        session['email'] = user.email
+        return jsonify({'message': 'Login successful', 'session': session}), 200
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -33,7 +36,7 @@ def register():
     form_role = data.get('role')
     user_exists = User.query.filter_by(email=form_email).first()
     if user_exists:
-        return jsonify({'message': 'user already exists'})
+        return jsonify({'message': 'user already exists'}), 403
     if form_role == 'farmer':
         form_location = data.get('location')
         form_phone = data.get('phone')
@@ -47,10 +50,18 @@ def register():
         db.session.commit()
         return jsonify({'message': 'Registration successful'}), 200
     elif form_role == 'buyer':
+        hashed_password = generate_password_hash(form_password)
         user = User(name=form_name,
                     emiail=form_email,
                     role=form_role,
-                    password_hash=user.set_password(form_password))
+                    password_hash=hashed_password)
         db.session.add(user)
         db.session.commit()
         return jsonify({'message': 'Registration successful'}), 200
+
+@app.route('/user/<int:id>', methods=['GET'])
+def user_info(id):
+    user = User.query.filter_by(id=id).first()
+    return jsonify({'id': user.id,
+                    'name': user.name,
+                    'email': user.email})
