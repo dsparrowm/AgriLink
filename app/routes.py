786c5c3dc904e513d273041a22ffc1aec28d2e70
Davies Aniefiok <daviesaniefiok32@gmail.com>
2023-05-23 02:53:02 -0700

    added update route  for updating user info

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from flask import request, jsonify
 from app import app
 from app.models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 
 @app.route('/status', methods=['GET'])
@@ -14,6 +15,9 @@ def status():
 
 @app.route('/login', methods=['POST'])
 def login():
+    """checks if user is valid and found in the database
+    then creates a jwt token for that user and returns a 
+    response to the client containing the the user token"""
     data = request.get_json()
     form_email = data.get('email')
     form_password = data.get('password')
@@ -21,8 +25,10 @@ def login():
     if user is None or check_password_hash(user.password_hash, form_password) != True:
         return jsonify({'message': 'Invalid email or password'}), 401
     else:
-        session['email'] = user.email
-        return jsonify({'message': 'Login successful', 'session': session}), 200
+        access_token = create_access_token(identity=user.id)
+        return jsonify({'message': 'Login successful',
+                        'token': access_token,
+                        'user_id': user.id}), 200
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -43,7 +49,9 @@ def register():
         user = User(name=form_name, email=form_email,
                     password_harsh=user.set_password(form_password))
         db.session.add(user)
+        #necessary to generate user_id used to create farmer object
         db.session.commit()
+
         farmer = Farmer(name=form_name,location=form_location,
                         phone=form_phone, user_id=user.id)
         db.session.add(farmer)
@@ -59,9 +67,47 @@ def register():
         db.session.commit()
         return jsonify({'message': 'Registration successful'}), 200
 
-@app.route('/user/<int:id>', methods=['GET'])
-def user_info(id):
-    user = User.query.filter_by(id=id).first()
+@app.route('/user', methods=['GET'])
+@jwt_required()
+def user_info():
+    """validates jwt token received from the client
+    and returns the user info to the client"""
+    current_user = get_jwt_identity()
+    user = User.query.get(current_user)
     return jsonify({'id': user.id,
                     'name': user.name,
-                    'email': user.email})
+                    'email': user.email,
+                    'role': user.role})
+
+@app.route('/updateUserInfo/<int:id>', methods=['PUT'])
+@jwt_required()
+def update_user(id):
+    current_user = get_jwt_identity()
+    if current_user != id:
+        return jsonify({'message': 'Unauthorized'}), 401
+    data = request.get_json()
+    user = User.query.get(id)
+    if not user:
+        return jsonify({'message': 'User Not Found'}), 404
+    if user.role == 'farmer':
+        farmer = Farmer.query.filter_by(user_id=user.id).first()
+        if 'name' in data:
+            user.name = data.get('name')
+            farmer.name = data.get('name')
+        if 'email' in data:
+            user.email = data.get('email')
+        if 'phone' in data:
+            farmer.phone = data.get('phone')
+        db.session.commit()
+        return jsonify({'message': 'user updated successfully',
+                        'user': user
+                        })
+    elif user.role == 'buyer':
+        if 'name' in data:
+            user.name = data.get('name')
+        if 'email' in data:
+            user.email = data.get('email')
+        db.session.commit()
+        return jsonify({'message': 'user updated successfully',
+                        'user': user
+                        })
